230873a29024191adbfb7818b154ec000dcca30b
Adrian Talonia <adrian.talonia11@gmail.com>
2026-04-30 22:55:41 -0600

    add initial jwt filter

--- src/main/java/com/atrdev/blogapp/service/AuthenticationService.java
+++ src/main/java/com/atrdev/blogapp/service/AuthenticationService.java
@@ -6,4 +6,5 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 public interface AuthenticationService {
     UserDetails authenticate(String email, String password);
     String generateToken(UserDetails userDetails);
+    UserDetails validateToken(String token);
 }
--- src/main/java/com/atrdev/blogapp/service/AuthenticationServiceImpl.java
+++ src/main/java/com/atrdev/blogapp/service/AuthenticationServiceImpl.java
@@ -1,15 +1,91 @@
 package com.atrdev.blogapp.service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+@RequiredArgsConstructor
 public class AuthenticationServiceImpl implements AuthenticationService {
+
+    private final AuthenticationManager authenticationManager;
+    private final UserDetailsService userDetailsService;
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    private final Long jwtExpirationMs = 8640000L;
+
     @Override
     public UserDetails authenticate(String email, String password) {
-        return null;
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));
+        return userDetailsService.loadUserByUsername(email);
     }
 
     @Override
     public String generateToken(UserDetails userDetails) {
-        return "";
+        Map<String, Object> claims = new HashMap<>();
+        return Jwts.builder()
+                .claims(claims)
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    @Override
+    public UserDetails validateToken(String token) {
+        String username = extractUsername(token);
+        return userDetailsService.loadUserByUsername(username);
+    }
+
+    /*public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }*/
+
+    /*private Key getSigningKey() {
+        byte[] keyBytes = secretKey.getBytes();
+        return Keys.hmacShaKeyFor(keyBytes);
+    }*/
+
+    private SecretKey getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> resolver) {
+        return resolver.apply(extractAllClaims(token));
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
     }
 }
